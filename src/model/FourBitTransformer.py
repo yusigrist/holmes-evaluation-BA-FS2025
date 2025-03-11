24a3440bc1f8d798d555cab258b6e2cc48ce42d8
Yutaro Sigrist <Yutaro.Sigrist@swisscom.com>
2025-03-11 19:50:02 +0100

    update fourbittransformer

--- src/model/FourBitTransformer.py
+++ src/model/FourBitTransformer.py
@@ -5,21 +5,24 @@ from transformers import AutoModel, T5Config, BitsAndBytesConfig
 
 class FourBitTransformer(Transformer):
 
-
-    def _load_model(self, model_name_or_path, config, cache_dir):
+    def _load_model(self, model_name_or_path, config, cache_dir, backend, is_peft_model, **model_args):
         """Loads the transformer model"""
         if isinstance(config, T5Config):
             self._load_t5_model(model_name_or_path, config, cache_dir)
         else:
-
             quantization_config = BitsAndBytesConfig(
                 load_in_4bit=True,
                 bnb_4bit_compute_dtype=torch.bfloat16
             )
+            self.auto_model = AutoModel.from_pretrained(
+                model_name_or_path, 
+                config=config, 
+                quantization_config=quantization_config, 
+                device_map="auto", 
+                trust_remote_code=True
+            )
 
-            self.auto_model = AutoModel.from_pretrained(model_name_or_path, config=config, quantization_config=quantization_config, load_in_4bit=True, device_map="auto", trust_remote_code=True)
-
-    def _load_t5_model(self, model_name_or_path, config, cache_dir):
+    def _load_t5_model(self, model_name_or_path, config, cache_dir, **model_args):
         """Loads the encoder model from T5"""
         from transformers import T5EncoderModel
         T5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
@@ -27,6 +30,10 @@ class FourBitTransformer(Transformer):
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.bfloat16
         )
-        self.auto_model = T5EncoderModel.from_pretrained(model_name_or_path, config=config,  quantization_config=quantization_config, load_in_4bit=True, device_map="auto", trust_remote_code=True)
-
-
+        self.auto_model = T5EncoderModel.from_pretrained(
+            model_name_or_path, 
+            config=config,  
+            quantization_config=quantization_config, 
+            device_map="auto", 
+            trust_remote_code=True
+        )

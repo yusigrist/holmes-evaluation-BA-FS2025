82e728d91380d3d203f84966cb2f685565abd7a2
Yutaro Sigrist <Yutaro.Sigrist@swisscom.com>
2025-04-21 13:51:54 +0200

    update fourbit

--- src/model/FourBitTransformer.py
+++ src/model/FourBitTransformer.py
@@ -12,7 +12,9 @@ class FourBitTransformer(Transformer):
         else:
             quantization_config = BitsAndBytesConfig(
                 load_in_4bit=True,
-                bnb_4bit_compute_dtype=torch.bfloat16
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,  # Enable for additional memory savings
+                bnb_4bit_compute_dtype=torch.bfloat16  # Match dtype to your training/inference setup
             )
             self.auto_model = AutoModel.from_pretrained(
                 model_name_or_path, 
@@ -27,9 +29,11 @@ class FourBitTransformer(Transformer):
         from transformers import T5EncoderModel
         T5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
         quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.bfloat16
-        )
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,  # Enable for additional memory savings
+                bnb_4bit_compute_dtype=torch.bfloat16  # Match dtype to your training/inference setup
+            )
         self.auto_model = T5EncoderModel.from_pretrained(
             model_name_or_path, 
             config=config,  
